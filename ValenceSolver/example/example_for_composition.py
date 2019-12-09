af62ea323e6099734fcf799da2985c86e1cb1bc0
zhugeyicixin <tanjin_he@berkeley.edu>
2019-12-08 23:41:49 -0800

    fix example

--- ValenceSolver/example/example_for_composition.py
+++ ValenceSolver/example/example_for_composition.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from pprint import pprint
 
-from pymatgen import Composition
-
 from ValenceSolver.core.composition_inhouse import CompositionInHouse
 from ValenceSolver.core.utils import to_GeneralMat_obj, get_material_valence
 
@@ -42,23 +40,18 @@ def get_valence_plain_dict(composition):
     valence_comp = CompositionInHouse(composition)
     valence_comp, inte_factor = valence_comp.get_integer_formula_and_factor()
     valence_comp = CompositionInHouse(valence_comp)
-    oxi_state, _, _ = valence_comp.oxi_state_guesses_most_possible()
+    oxi_state = valence_comp.oxi_state_guesses_most_possible(all_oxi_states=False)
     print(composition, oxi_state)
     return oxi_state
 
-
-def get_valence_plain_formula(formula, mode='inhouse'):
-    if mode == 'pymatgen':
-        valence_comp = Composition(formula)
-        valence_comp, inte_factor = valence_comp.get_integer_formula_and_factor()
-        valence_comp = Composition(valence_comp)
-        oxi_state = valence_comp.oxi_state_guesses()
-    else:
-        valence_comp = CompositionInHouse(formula)
-        valence_comp, inte_factor = valence_comp.get_integer_formula_and_factor()
-        valence_comp = CompositionInHouse(valence_comp)
-        oxi_state, is_usual, comments = valence_comp.oxi_state_guesses_most_possible()
-
+def get_valence_plain_formula(formula, add_zero_valence=False):
+    valence_comp = CompositionInHouse(formula)
+    valence_comp, inte_factor = valence_comp.get_integer_formula_and_factor()
+    valence_comp = CompositionInHouse(valence_comp)
+    oxi_state = valence_comp.oxi_state_guesses_most_possible(
+        all_oxi_states=False,
+        add_zero_valence=add_zero_valence
+    )
     print(formula, oxi_state)
     return oxi_state
 
@@ -69,14 +62,45 @@ if __name__ == '__main__':
         "Fe": 1.0,
         "O": 3.0,
     }
-    get_valence_plain_dict(composition)
-    add_zero_valence = True
-    get_valence_plain_formula('YFeO3')
-    get_valence_plain_formula('Y0.1Fe1.9O3')
-    get_valence_plain_formula('Y0.1Fe2O3')
-    get_valence_plain_formula('Y0.2Fe2O3')
-    get_valence_plain_formula('Y0.5Fe2O3')
-    get_valence_plain_formula('Fe1.9Y2.0O3')
-    get_valence_plain_formula('YFe2O3')
-    get_valence_plain_formula('Eu0.1Y2O3')
-
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition(composition)
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('YFeO3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('Y0.1Fe1.9O3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('Y0.1Fe2O3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('Y0.2Fe2O3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('Y0.5Fe2O3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('Fe1.9Y2.0O3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('YFe2O3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('Eu0.1Y2O3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('Nb(PO4)3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('NaNb0.667Cr0.667(PO4)3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('Na2Nb0.333Cr1.333(PO4)3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('Na2.5Nb0.1667Cr1.6667(PO4)3')
+    )
+    print(
+        CompositionInHouse.get_most_possible_oxi_state_of_composition('Na3Cr2(PO4)3')
+    )
